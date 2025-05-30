8b845fc8ee591f0d06b484d2cea501d972ee4476
Din Ganic <dinganic03@gmail.com>
2025-05-30 08:28:53 -0400

    Added confirm order method

--- src/main/java/com/pluralsight/Checkout.java
+++ src/main/java/com/pluralsight/Checkout.java
@@ -1,5 +1,11 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class Checkout {
@@ -153,4 +159,66 @@ public class Checkout {
     }
 
 
+    public static void confirmsOrder(Order order){
+        try {
+
+            // Creates formatter with proper format; formats current datetime using this format
+            DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
+            String formattedTimeDate = LocalDateTime.now().format(dtFormatter);
+
+            // Specifies receipt file in receipt folder
+            File file = new File("receipts/" + formattedTimeDate + ".txt");
+
+            // Creates new receipt file
+            file.createNewFile();
+
+            // Opens the receipt file
+            FileWriter fileWriter = new FileWriter(file, false); // "true" appends the file instead of overwriting
+
+            // Creates a BufferedWriter
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            // Writes to the file
+            for (Sandwich sandwich : order.getSandwiches()){
+                bufWriter.write(sandwich.getBread() + "; ");
+                bufWriter.write(sandwich.getSize() + "; ");
+
+                for (String meat : sandwich.getToppings().getMeats()){
+                    bufWriter.write(meat + "; ");
+                }
+
+                for (String cheese : sandwich.getToppings().getCheeses()){
+                    bufWriter.write(cheese + "; ");
+                }
+
+                for (String otherTopping : sandwich.getToppings().getOtherToppings()){
+                    bufWriter.write(otherTopping + "; ");
+                }
+
+                for (String sauce : sandwich.getToppings().getSauces()){
+                    bufWriter.write(sauce + "; ");
+                }
+
+                bufWriter.write((sandwich.isToasted()) ? "Toasted" : "Not Toasted");
+
+            }
+
+            for (Drink drink : order.getDrinks()){
+                bufWriter.write(drink.getFlavor() + "; ");
+                bufWriter.write(drink.getSize() + "; ");
+            }
+
+            for (String singularChips : order.getChips()){
+                bufWriter.write(singularChips);
+            }
+
+            // close the file when you are finished using it
+            bufWriter.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("ERROR: An unexpected error occurred");
+            e.getStackTrace();
+        }
+    }
 }
